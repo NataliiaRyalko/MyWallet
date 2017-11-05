57ef0a41126dcccc40650515345bb13640ed1d87
KhvorostyanyiViacheslav <slavafh@gmail.com>
2017-11-05 22:17:07 +0200

    just some additions

--- sandbox_dict.py
+++ sandbox_dict.py
@@ -1,12 +1,12 @@
 
 import datetime
-
+from tkinter import *
 date_time = datetime.datetime.now().strftime("%H:%M:%S %d-%m-%y")
 
 account = {}
 account["cash"] = 1000
 
-print account
+print (account)
 
 transaction = {}
 
@@ -16,11 +16,25 @@ transaction["category"] = "test_category"
 transaction_stack = {}
 transaction_stack[date_time] = transaction
 
-def calc(account_v,transaction_value):
-	calc  = account_v - transaction_value
+def calc(account_v,transaction_v):
+	calc  = account_v - transaction_v
 	account['cash'] = calc
 	
 calc(account['cash'], transaction['value'])	
-print transaction_stack
+print (transaction_stack)
 
-print account
+print (account)
+
+root = Tk()
+# root.geometry('200X300')
+entry = Entry(root)
+entry.pack()
+def callback():
+	tr_value = entry.get()
+	transaction_stack[date_time] = transaction
+	transaction["value"] = tr_value
+	print(transaction_stack)
+	print(date_time)
+btn = Button(root, text = 'get',command = callback)
+btn.pack()
+root.mainloop()
