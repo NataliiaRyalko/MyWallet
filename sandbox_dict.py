7405ab0e0dd8d405fdbd768b107fd3ac13656407
KhvorostyanyiViacheslav <slavafh@gmail.com>
2017-11-06 07:28:55 +0200

    add label and time get

--- sandbox_dict.py
+++ sandbox_dict.py
@@ -1,7 +1,7 @@
 
 import datetime
 from tkinter import *
-date_time = datetime.datetime.now().strftime("%H:%M:%S %d-%m-%y")
+date_value = datetime.datetime.now().strftime("%d-%m-%y")
 
 account = {}
 account["cash"] = 1000
@@ -14,7 +14,7 @@ transaction["value"] = 100
 transaction["discription"] = "it's transaction"
 transaction["category"] = "test_category"
 transaction_stack = {}
-transaction_stack[date_time] = transaction
+transaction_stack[date_value] = transaction
 
 def calc(account_v,transaction_v):
 	calc  = account_v - transaction_v
@@ -26,15 +26,26 @@ print (transaction_stack)
 print (account)
 
 root = Tk()
-# root.geometry('200X300')
+root.geometry('200x300')
 entry = Entry(root)
 entry.pack()
+
+import time
+def tick():
+    label.after(200, tick)
+    label['text'] = time.strftime('%H:%M:%S')
+label = Label(root,font='sans 20')
+label.pack()
+label.after_idle(tick)
+
 def callback():
+	time_value = label['text']
 	tr_value = entry.get()
-	transaction_stack[date_time] = transaction
+	transaction_stack[time_value] = transaction
 	transaction["value"] = tr_value
 	print(transaction_stack)
-	print(date_time)
+	print(time_value)
+
 btn = Button(root, text = 'get',command = callback)
 btn.pack()
 root.mainloop()
